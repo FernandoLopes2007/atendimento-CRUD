e3296022a25e50c12e648d1a3161c8ac287ec193
fernando <fernando.lopes48@etec.sp.gov.br>
2025-11-18 14:21:02 -0300

    teste

---
+++ atendimento-CRUD/atendimento-CRUD/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace atendimento_CRUD
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string escolha;
+
+            while (true)
+            {
+                Console.WriteLine("\nBem-vindo ao cadastro de paciente\n");
+                Console.WriteLine("1 - Cadastro\n2 - Lista\n3 - Atender\n4 - Alterar\nQ - Sair\n");
+                escolha = Console.ReadLine();
+
+                switch (escolha)
+                {
+                    case "1":
+                        Console.WriteLine("\nÁrea de Cadastro\n");
+                        break;
+
+                    case "2":
+                        Console.WriteLine("\nLista de Pacientes\n");
+                        break;
+
+                    case "3":
+                        Console.WriteLine("\nAtender Pacientes\n");
+                        break;
+
+                    case "4":
+                        Console.WriteLine("\nAlterar Dados\n");
+
+                        break;
+
+                    case "q":
+                        if (escolha == "q" || escolha == "Q")
+                        {
+                            Console.WriteLine("\nPrograma encerrado");
+                        }
+                        return;
+
+                    default:
+                        Console.WriteLine("\nOpção Incorreta");
+                        break;
+                }
+            }
+        }
+    }
+}
---
+++ atendimento-CRUD/atendimento-CRUD/paciente.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace atendimento_CRUD
+{
+    internal class paciente
+    {
+        public string Nome;
+        public int Prioridade; //0 - Baixo, 1 - Médio 2 - Alto
+        public int CdPaciente;
+
+        public void CadastrarPaciente()
+        {
+            Console.WriteLine("Digite o nome do paciente");
+            Nome = Console.ReadLine();
+            do
+            {
+                Console.WriteLine("Digite o nível de prioridade do paciente");
+                Console.WriteLine("0 - Baixo\n1 - Médio\n2 - Alto");
+                Prioridade = int.Parse(Console.ReadLine());
+                if (Prioridade >= 3)
+                {
+                    Console.WriteLine("\nDigite um valor valido\n");
+                }
+            } while (Prioridade >= 3);
+        }
+        public void ListarDados()
+        {
+            Console.WriteLine("Nível de Prioridade: {1}, {0} ", Prioridade, Nome);
+        }
+    }
+}
