6d0413cfdcd530389944a8a4aa95c790f1030b2f
fernando <fernando.lopes48@etec.sp.gov.br>
2025-11-18 15:15:25 -0300

    Atualização

--- atendimento-CRUD/atendimento-CRUD/Program.cs
+++ atendimento-CRUD/atendimento-CRUD/Program.cs
@@ -1,17 +1,21 @@
-﻿using System;
+﻿using MySql.Data.MySqlClient;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
-using MySql.Data.MySqlClient;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace atendimento_CRUD
 {
     internal class Program
     {
+        static string conexao = "server=127.0.0.1;uid=root;pwd=root;database=atendimentoCRUD";
         static void Main(string[] args)
         {
             string escolha;
+
 
             while (true)
             {
@@ -23,19 +27,21 @@ namespace atendimento_CRUD
                 {
                     case "1":
                         Console.WriteLine("\nÁrea de Cadastro\n");
+                        CadastrarPaciente();
                         break;
 
                     case "2":
                         Console.WriteLine("\nLista de Pacientes\n");
+                        ListarPaciente();
                         break;
 
                     case "3":
                         Console.WriteLine("\nAtender Pacientes\n");
+                        AtenderPaciente();
                         break;
 
                     case "4":
                         Console.WriteLine("\nAlterar Dados\n");
-
                         break;
 
                     case "q":
@@ -49,6 +55,54 @@ namespace atendimento_CRUD
                         Console.WriteLine("\nOpção Incorreta");
                         break;
                 }
+            }
+        }
+
+        static void CadastrarPaciente()
+        {
+            Console.WriteLine("Digite o nome do paciente:");
+            string Nome = Console.ReadLine();
+            Console.WriteLine("Digite o nível de prioridade do paciente");
+            int Prioridade = int.Parse(Console.ReadLine());
+
+            using (var con = new MySqlConnection(conexao))
+            {
+                con.Open();
+                var cmd = new MySqlCommand("insert into paciente (Nome, Prioridade) values (@Nome, @Prioridade)", con);
+                cmd.Parameters.AddWithValue("@Nome", Nome);
+                cmd.Parameters.AddWithValue("@Prioridade", Prioridade);
+                cmd.ExecuteNonQuery();
+                Console.WriteLine("Paciente cadastrado.");
+            }
+        }
+        static void ListarPaciente()
+        {
+            using (var con = new MySqlConnection(conexao))
+            {
+                con.Open();
+                string sqlSelect = "select * from paciente order by Prioridade asc";
+                var cmd = new MySqlCommand(sqlSelect, con);
+                var rdr = cmd.ExecuteReader();
+
+                while (rdr.Read())
+                {
+                    Console.WriteLine(rdr[0]+" - " + rdr[1]+ " Prioridade: " + rdr[2]);
+                }
+            }
+        }
+        static void AtenderPaciente()
+        {
+            ListarPaciente();
+            Console.WriteLine("Digite o ID do paciente a ser atendido");
+            int cd = int.Parse(Console.ReadLine());
+            using (var con = new MySqlConnection(conexao))
+            {
+                con.Open();
+                string sqlDelete = "Delete from Paciente where CdPaciente = @cd";
+                var cmd = new MySqlCommand(sqlDelete, con);
+                cmd.Parameters.AddWithValue("@cd", cd);
+                int removidos = cmd.ExecuteNonQuery();
+                Console.WriteLine($"{removidos}");
             }
         }
     }
